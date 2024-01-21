947d30b4c0d09fc1806185be31363bb372ffc493
Jonathan Hartley <jonathan@lambdal.com>
2024-01-20 21:17:36 -0600

    Cities can be drawn intact or destroyed
    Destroyed is as-yet unused.

--- city.gd
+++ city.gd
@@ -3,11 +3,11 @@ extends Node2D
 const COLUMNS := 7
 const SIZE := 100.0
 
-var verts: Array[Vector2]
+var destroyed: bool = false
 
-func get_verts() -> Array[Vector2]:
+func get_regular_verts():
     var retval: Array[Vector2] = []
-    var heights:Array[int] = [20, 30, 40, 40, 50, 50, 60]
+    var heights:Array[int] = [20, 20, 30, 40, 40, 50, 50]
     heights.shuffle()
     retval.append(Vector2(-SIZE/2.0, 0))
     for column in range(COLUMNS):
@@ -16,11 +16,26 @@ func get_verts() -> Array[Vector2]:
     retval.append(Vector2(+SIZE/2.0, 0))
     return retval
 
-func _init():
-    self.verts = get_verts()
+func get_destroyed_verts():
+    var retval: Array[Vector2] = []
+    var heights:Array[int] = [10, 10, 20, 20, 20, 30, 30]
+    heights.shuffle()
+    retval.append(Vector2(-SIZE/2.0, 0))
+    for column in range(COLUMNS):
+        retval.append(Vector2(-SIZE/2.0 + column * SIZE/COLUMNS, heights[column]))
+    retval.append(Vector2(+SIZE/2.0, 0))
+    return retval
 
 func _ready() -> void:
     self.rotation = position.angle() - PI / 2
 
 func _draw():
-    draw_polyline(verts, Color(1, .3, .3), 4.0, true)
+    var verts:Array[Vector2]
+    var color:Color
+    if self.destroyed:
+        verts = get_destroyed_verts()
+        color = Color(.4, .4, .4)
+    else:
+        verts = get_regular_verts()
+        color = Color.RED
+    draw_polyline(verts, color, 4.0, true)
