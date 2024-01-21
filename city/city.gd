403271391c2aa955a0da81863ef00ba8a9c19239
Jonathan Hartley <jonathan@lambdal.com>
2024-01-20 17:55:59 -0800

    Consistent use of typed ints and floats

--- city/city.gd
+++ city/city.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const COLUMNS = 7
-const SIZE = 100
+const COLUMNS := 7
+const SIZE := 100.0
 
 var verts: Array[Vector2]
 
@@ -9,11 +9,11 @@ func get_verts() -> Array[Vector2]:
     var retval: Array[Vector2] = []
     var heights:Array[int] = [20, 30, 40, 40, 50, 50, 60]
     heights.shuffle()
-    retval.append(Vector2(-SIZE/2, 0))
+    retval.append(Vector2(-SIZE/2.0, 0))
     for column in range(COLUMNS):
-        retval.append(Vector2(-SIZE/2 + column * SIZE/COLUMNS, heights[column]))
-        retval.append(Vector2(-SIZE/2 + (column + 1) * SIZE/COLUMNS, heights[column]))
-    retval.append(Vector2(+SIZE/2, 0))
+        retval.append(Vector2(-SIZE/2.0 + column * SIZE/COLUMNS, heights[column]))
+        retval.append(Vector2(-SIZE/2.0 + (column + 1) * SIZE/COLUMNS, heights[column]))
+    retval.append(Vector2(+SIZE/2.0, 0))
     return retval
 
 func _init():
