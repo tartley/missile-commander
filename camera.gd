a4ccddfdd2034b23d831cef89f61c5d710f6eea2
Jonathan Hartley <jonathan@lambdal.com>
2024-01-20 18:19:35 -0800

    Noodle around with camera placement a bit

--- camera.gd
+++ camera.gd
@@ -1,12 +1,13 @@
 extends Camera2D
 
+# Need injecting by a parent
 var mouse:Node2D
 var ground:Node2D
 
 func _process(_delta):
     var radius:float = ground.RADIUS + get_viewport_rect().size.y * 0.4
-    rotation = - ground.PLANET_ANGLE * 0.64 * mouse.normalized.x
     position = Vector2(
         -radius * sin(rotation),
-         radius * cos(rotation) + get_viewport_rect().size.y * mouse.normalized.y * 0.1
+        radius * cos(rotation) + get_viewport_rect().size.y * mouse.normalized.y * 0.1
     )
+    rotation = ground.PLANET_ANGLE * -0.64 * mouse.normalized.x
