22d6facc0304a61f394dfc0f09e21e28dda93ecd
Jonathan Hartley <tartley@tartley.com>
2024-01-24 23:12:57 -0600

    Don't need a new RNG, can use the global one

--- sky.gd
+++ sky.gd
@@ -1,9 +1,5 @@
 extends Node2D
 
-const SCROLL_SPEED := 0.1
-
-var _random = RandomNumberGenerator.new()
-
 func _draw():
     var verts: Array[Vector2] = []
     var colors: Array[Color] = []
@@ -12,13 +8,13 @@ func _draw():
     var size:float
     var bright: float
     for i in range(100):
-        x = _random.randf_range(-2800.0, 2800.0)
-        y = _random.randf_range(
+        x = randf_range(-2800.0, 2800.0)
+        y = randf_range(
             %Ground.RADIUS - get_viewport_rect().size.y * 0.14,
             %Ground.RADIUS + get_viewport_rect().size.y * 1.04
         )
-        size = _random.randf_range(4.0, 8.0)
-        bright = _random.randf_range(0.5, 1.0)
+        size = randf_range(4.0, 8.0)
+        bright = randf_range(0.5, 1.0)
         verts.append(Vector2(x, y))
         verts.append(Vector2(x + size, y))
         colors.append(Color(bright, bright, bright))
