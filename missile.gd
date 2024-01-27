66b27886ee95e5d9d167e682220e4a5e1d413ab9
Jonathan Hartley <tartley@tartley.com>
2024-01-26 22:29:17 -0600

    Tweak drawing of missiles and their trails
    * Trails now eminate from the rear end of the missile, not their center
      point.
    * Missiles now render in front of the trails, with a black body.

--- missile.gd
+++ missile.gd
@@ -21,6 +21,7 @@ func set_up_collisions():
     add_child(collision)
 
 func _ready() -> void:
+    $Trail.position = Vector2(0, SIZE/2)
     $Trail.direction = Vector2(0, 1)
     $Trail.initial_velocity_max = velocity.length() / 5
     $Trail.initial_velocity_min = velocity.length() / 5
@@ -35,6 +36,7 @@ func _process(delta: float) -> void:
     self.position += velocity * delta
 
 func _draw():
+    draw_polygon(verts, [Color.BLACK])
     draw_polyline(verts, Color(.8, 7, .4), 2.0, true)
 
 func on_area_entered(ground:Area2D):
