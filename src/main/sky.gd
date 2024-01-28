3de66ee235f9b3dd4b5488bd1fde593df4b4ffa2
Jonathan Hartley <tartley@tartley.com>
2024-01-27 23:23:51 -0600

    Tweak star colors, sizes

--- src/main/sky.gd
+++ src/main/sky.gd
@@ -8,16 +8,17 @@ func _draw():
     var x: float
     var y: float
     var size:float
-    var bright: float
     for i in range(1000):
         x = randf_range(-2800.0, 2800.0)
         y = randf_range(
             ground.RADIUS - get_viewport_rect().size.y * 0.14,
             ground.RADIUS + get_viewport_rect().size.y * 1.04
         )
-        size = randf_range(4.0, 8.0)
-        bright = randf_range(0.5, 1.0)
+        size = randf_range(2.0, 6.0)
         verts.append(Vector2(x, y))
         verts.append(Vector2(x + size, y))
-        colors.append(Color(bright, bright, bright))
-    draw_multiline_colors(verts, colors, size)
+        var red = randf_range(0.0, 1.0) ** 0.2
+        var blue = randf_range(0.0, 1.0) ** 0.2
+        var green = min(red, blue)
+        colors.append(Color(red, green, blue))
+    draw_multiline_colors(verts, colors, 4.0)
