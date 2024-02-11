1291ac460970f8dd1d9158e51464232c5bf7fa49
Jonathan Hartley <tartley@tartley.com>
2024-02-11 08:27:45 -0600

    Tweak BangFeature appearance

--- src/bang_feature/bang_feature.gd
+++ src/bang_feature/bang_feature.gd
@@ -7,11 +7,9 @@ const SIZE := 250.0 # world co-ords
 const DURATION := 3.0 # seconds
 
 var progress:float # 0..1
-var on:bool
 
 func _ready() -> void:
     self.name = Common.get_unique_name(self)
-    self.on = false
 
 func _process(delta:float) -> void:
     self.progress += delta / DURATION
@@ -20,12 +18,6 @@ func _process(delta:float) -> void:
         queue_free()
 
 func _draw() -> void:
-    var color:Color
-    if self.on:
-        color = Color(1, 1 - self.progress / 2, 1, 1 - self.progress)
-    else:
-        color = Color.BLACK
-    self.on = not self.on
-    # var width := 1 + 20.0 * (1 - progress)
+    var color := Color(1, 1 - self.progress / 2, 1, 1 - self.progress)
     for i in range(4):
-        draw_arc(Vector2.ZERO, SIZE * i * self.progress ** 0.5, -PI, PI, 16, color, 3.0, true)
+        draw_arc(Vector2.ZERO, SIZE * (i + 1) * self.progress ** 0.5, -PI, PI, 16, color, (4 - i) * 6.0, true)
