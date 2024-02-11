e812b1b12a133b96980c76605e996a4a7f1b3844
Jonathan Hartley <tartley@tartley.com>
2024-02-11 10:00:12 -0600

    Wider base shape

--- src/base/base.gd
+++ src/base/base.gd
@@ -3,7 +3,7 @@ extends Node2D
 const BangFeatureScene:PackedScene = preload("res://src/bang_feature/bang_feature.tscn")
 const ShotScene:PackedScene = preload("res://src/shot/shot.tscn")
 
-const SIZE := 100.0
+const SIZE := 75.0
 const FORE := Color.YELLOW
 const FORE_DESTROYED := Color(.3, .3, .3)
 const FILL := Color.BLACK
@@ -16,7 +16,8 @@ var destroyed:bool = false
 
 func _ready():
     self.name = Common.get_unique_name(self)
-    $Turret.position = Vector2(0, SIZE / 4)
+    $Turret.size = SIZE
+    $Turret.position = Vector2(0, SIZE * 3/8)
     self.add_to_group("bases")
 
 func _process(_delta:float):
@@ -27,27 +28,29 @@ func _process(_delta:float):
 
 func _draw():
     draw_polygon(self.verts, [FILL])
-    draw_polyline(self.verts, self.color, 3.0, true)
+    draw_polyline(self.verts, self.color, 2.0, true)
 
-func get_semicircle(center, radius, segments) -> Array[Vector2]:
+func get_dome(center, radius, segments) -> Array[Vector2]:
     var vs:Array[Vector2] = []
     for segment in range(segments + 1):
-        var angle:float = PI - PI * segment / segments
+        var angle:float = PI * 3/4 - PI/2 * segment / segments
         var vert:Vector2 = center + Vector2.from_angle(angle) * radius
         vs.append(vert)
     return vs
 
 func get_verts() -> Array[Vector2]:
-    const DOME_SEGMENTS := 5
+    const DOME_SEGMENTS := 10
     var vs:Array[Vector2] = []
     vs.append_array([
-        Vector2(-SIZE/4, 0), # support left base
-        Vector2(-SIZE/4, SIZE/4), # support left top
+        Vector2(-SIZE*5/8, 0), # support left base
+        Vector2(-SIZE*3/8, SIZE/4), # support left corner
+        Vector2(-SIZE*2/8, SIZE/4), # left edge of dome
     ])
-    vs.append_array(get_semicircle(Vector2(0, SIZE/4), SIZE/4, DOME_SEGMENTS))
+    vs.append_array(get_dome(Vector2(0, SIZE/4), SIZE/4, DOME_SEGMENTS))
     vs.append_array([
-        Vector2(+SIZE/4, SIZE/4), # suppport right top
-        Vector2(+SIZE/4, 0), # support right base
+        Vector2(+SIZE*2/8, SIZE/4), # left edge of dome
+        Vector2(+SIZE*3/8, SIZE/4), # suppport right corner
+        Vector2(+SIZE*5/8, 0), # support right base
     ])
     return vs
 
--- src/base/turret.gd
+++ src/base/turret.gd
@@ -1,29 +1,30 @@
 extends Node2D
 
-const SIZE := 100.0
 const FORE := Color.YELLOW
 const FORE_DESTROYED := Color(.3, .3, .3)
 const FILL := Color.BLACK
 
+var size:float
 var destroyed:bool = false
 var verts:Array[Vector2]
 var color:Color
 
-func _init():
+func _ready():
+    self.size = get_parent().SIZE
     self.verts = get_verts()
     self.color = FORE
 
 func _draw():
     draw_polygon(self.verts, [FILL])
-    draw_polyline(self.verts, self.color, 3.0)
+    draw_polyline(self.verts, self.color, 2.0, true)
 
 func get_verts() -> Array[Vector2]:
     # A rectangular gun turret, pointing +x
     return [
-        Vector2(     0,  SIZE/16), # left(base) top
-        Vector2(SIZE/2,  SIZE/16), # right(tip) top
-        Vector2(SIZE/2, -SIZE/16), # right(tip) bottom
-        Vector2(     0, -SIZE/16), # left(base) bottom
+        Vector2(     0,  size/16), # left(base) top
+        Vector2(size/2,  size/16), # right(tip) top
+        Vector2(size/2, -size/16), # right(tip) bottom
+        Vector2(     0, -size/16), # left(base) bottom
     ]
 
 func destroy() -> void:
